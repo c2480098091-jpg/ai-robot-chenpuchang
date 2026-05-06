460232e8d63d399327454f267754873b4fef8e9e
chenpuchang <c2480098091@gmail.com>
2026-05-06 14:22:16 +0900

    update

--- scripts/nano test.py
+++ scripts/nano test.py
@@ -1,8 +1,18 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread("下载.png")
+# 读取本地图片（注意路径）
+img = cv2.imread("/home/chenpuuuu/ai-robot-chenpuchang/作业/week10/image.png")
 
+# 显示彩色图
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.axis("off")
+plt.show()
+
+# 转灰度图
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 显示灰度图
+plt.imshow(img_gray, cmap="gray")
 plt.axis("off")
 plt.show()
